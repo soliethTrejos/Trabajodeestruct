1ae8636eb25e299869148d35fb713813d6f5c311
ZaNtYsZ <ssmena@uamv.edu.ni>
2024-05-24 08:41:38 -0600

    santiagpo

---
+++ ejer_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void convertirNumeros() {
+    int numeros[15];
+
+    cout << "Ingrese 15 numeros negativos:\n";
+
+    for (int i = 0; i < 15; i++) {
+        cin >> numeros[i];
+
+        numeros[i] = -numeros[i];
+    }
+
+    cout << "Numeros convertidos a positivos:\n";
+
+    for (int i = 0; i < 15; i++) {
+        cout << numeros[i] << " ";
+    }
+
+    cout << endl;
+}
