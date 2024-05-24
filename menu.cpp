cf04d3ee799e06d063573c390a2ac87216cbcf07
soli.tr4 <164054168+soliethTrejos@users.noreply.github.com>
2024-05-24 09:13:14 -0600

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -3,9 +3,28 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+int main()
 {
-     convertirNumeros();
+    int ejer;
+    cout << "Ingrese el ejercicio a conocer: "<<endl;
+    cin >> ejer;
+    cout<<endl;
 
+    if (ejer == 1)
+    {
+        ejercicio1();
+    }
+    else if (ejer == 2)
+    {
+        ejercicio2();
+    }
+    else if (ejer == 3)
+    {
+        ejercicio3();
+    }
+    else
+    {
+        cout << "No se encuentra el ejercicio";
+    }
     return 0;
 }
